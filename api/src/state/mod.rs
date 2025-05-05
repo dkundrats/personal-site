22df2cf7e95f455bc4535c05317e2ace2aa6b8df
David Kundrats <david.kundrats@anikasystems.com>
2025-05-05 17:05:06 -0400

    move bucket name to .env

--- api/src/state/mod.rs
+++ api/src/state/mod.rs
@@ -2,28 +2,24 @@ use aws_sdk_s3::Client as S3Client;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
-use std::env;
 
 #[derive(Clone)]
 pub struct AppState {
     pub s3_client: Arc<S3Client>,
     pub cache: Arc<RwLock<HashMap<String, String>>>,
-    pub bucket_name: String
 }
 
 impl AppState {
     pub async fn new() -> Result<Self, String> {
-        let config = aws_config::load_from_env().await;
+        // Load config from environment variables (which should now include those from .env)
+        let config = aws_config::from_env().load().await;
         let s3_client = Arc::new(S3Client::new(&config));
         
         let cache = Arc::new(RwLock::new(HashMap::new()));
-	 let bucket_name = env::var("S3_BUCKET_NAME")
-            .map_err(|_| "S3_BUCKET_NAME environment variable not set".to_string())?;
-
-	Ok(Self {
+         
+        Ok(Self {
             s3_client,
-            cache,
-	    bucket_name
+            cache
         })
     }
 }
